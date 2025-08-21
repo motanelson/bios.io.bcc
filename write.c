d6acc47c84c42ee6d2eac8dcaa0c8bde72d6067c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-22 00:28:56 +0100

    Add files via upload

---
+++ write.c
@@ -0,0 +1,68 @@
+#define varn 0x0080
+//bcc -Md -o stdout.com stdout.c
+void pputc(c)
+char c;
+{
+
+    sputc(c);
+
+}
+void pputs(s)
+char *s;
+{
+    int n=0;
+    char *ss=s;
+    while(s[n]!=0){
+        ss=s+n;
+        pputc(ss);
+        n++;
+    }
+
+}
+int main(){
+    int nn;
+    int n;
+    n=1;
+    nn=0xe020;
+    cls3(nn);
+    pputs("hello world......\n\n");	
+    return 0;
+}
+#asm
+.globl _cls3
+.globl _sputc 
+_cls3:
+    mov ax,*03
+    int *0x10
+    mov si,sp
+    add si,*0x2
+    mov dx,[si]
+    mov ax,*0xb800
+    push ds
+    mov ds,ax
+    
+    mov ax,dx
+    mov cx,*0x8a0
+    mov si,*0x0
+    
+cls31:
+    
+    mov [si],ax   
+    inc si
+    inc si
+    dec cx
+    cmp cx,*0x0
+    jnz cls31
+    pop ds
+    ret
+_sputc:
+    mov si,sp
+    add si,*0x2
+    mov bx,*01
+    mov cx,*01
+    mov dx,[si]
+    mov ah,*0x40
+    int *0x21
+    ret
+
+#endasm
